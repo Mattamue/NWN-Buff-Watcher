c3ca5064ce9bb5b7fa742601cbd38f9ef2efc609
Mattamue <Mattamue@Gmail.com>
2021-05-03 22:24:51 -0500

    Removed testing dir from cache, just a bunch of examples for my reference

--- testing/1tk_hello_pack.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-root = Tk()
-
-myLabel = Label(root, text = "Hello World!")
-
-myLabel.pack()
-
-root.mainloop()
--- testing/test_image.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *      
-root = Tk()      
-canvas = Canvas(root)      
-canvas.pack()      
-img = PhotoImage(file="arelith_ui\Is_bullstr.gif")      
-canvas.create_image(0, 0, anchor=NW, image=img)      
-mainloop()   
-
